dddc3d94aec118b12bb85c45f4b2448bca92dffe
Nedjeljko <nedjeljko.miloslavic94@gmail.com>
2022-05-16 13:41:51 +0200

    gifs first commit

--- app.js
+++ app.js
@@ -1,270 +1,173 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 1000,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-};
+const canvas = document.querySelector("canvas")
+canvas.width = 1200
+canvas.height = 600
 
-var game = new Phaser.Game(config);
+const c = canvas.getContext("2d")
+c.font = "25px Arial"
 
+//----CLASSES-----
+//this is generic background class (for sky and ground)
 
-let startGameText = "name: \n\nThis game is about collecting stars \nand avoiding the 'bomb' that is jumping \naround the screen. You have 3 lives \nand every time you get hit by bomb \nyou lose one life. When losing all, game is over!"
-function preload ()
-{
-    this.load.image('sky', 'assets/sky.png');
-    this.load.image("city", "assets/city.webp")
-    this.load.image("grass", "assets/grass.webp")
-    this.load.atlas("car", "assets/car.png", "assets/sprites.json")
-    this.load.spritesheet("helicopter", "assets/helicopter.png", {frameWidth:400,frameHeight:150})
-    this.load.spritesheet("person", "assets/person.png", {frameWidth:83,frameHeight:144})
-    
-}
-
-let car
-let helicopter
-let person
-let helicopterMove = {
-    move:false,
-    moveRight:false,
-    moveLeft:false
-}
-
-let carMove = {
-    move:false,
-    forward:false,
-    backwards:false,
-    turnLeft:false,
-    turnRight:false,
-
-}
-
-function create (){
-    this.add.image(500,300,"sky").setScale(2)
-    this.add.image(500,600,"grass").setScale(3,1)
-    this.add.image(500,300,"city")
-
-    car = this.physics.add.sprite(100,100,"car").setScale(1.5)
-    helicopter = this.physics.add.sprite(100,170,"helicopter").setScale(0.3)
-    person = this.add.sprite(100,250,"person").setScale(0.6)
-
-    //person anims
-    this.anims.create({
-        key:"person",
-        frames:this.anims.generateFrameNumbers("person", {start:0,end:10}),
-        repeat:-1,
-        frameRate:3
-    })
-   
-
-
-    //car anims
-    this.anims.create({
-        key:"forward",
-        frames:this.anims.generateFrameNames("car", {prefix:"sprite",start:3,end:7, zeroPad:1}),
-        repeat:1,
-        frameRate:3
-    })
-
-    this.anims.create({
-        key:"sideways",
-        frames:this.anims.generateFrameNames("car", {prefix:"sprite",start:7,end:7, zeroPad:1}),
-    })
-
-    //helicopter anims
-    this.anims.create({
-        key:"helicopter",
-        frames: this.anims.generateFrameNumbers("helicopter", {start:0,end:2}),
-        repeat:-1,
-        frameRate:15
-    })
-    car.anims.play("sideways")
-    
-
-    //controlling the dragging process
-    person.setInteractive()
-    this.input.setDraggable(person)
-    person.on("pointerover", ()=>{
-        person.setTint(0x44ff44)
-    })
-    person.on("pointerout", ()=>{
-        person.clearTint()
-    })
-
-    helicopter.setInteractive()
-    this.input.setDraggable(helicopter)
-    helicopter.on("pointerover", ()=>{
-        helicopter.setTint(0x44ff44)
-    })
-    helicopter.on("pointerout", ()=>{
-        helicopter.clearTint()
-    })
-
-    car.setInteractive()
-    this.input.setDraggable(car)
-    car.on("pointerover", ()=>{
-        car.setTint(0x44ff44)
-    })
-    car.on("pointerout", ()=>{
-        car.clearTint()
-    })
-
-
-    this.input.on("drag", (pointer,gameObject,dragX,dragY)=>{
-        gameObject.x = dragX
-        gameObject.y = dragY
-    })
-    this.input.on("dragend", (pointer,gameObject,target)=>{
-        if(gameObject===car) placeCar()
-        if(gameObject===helicopter) placeHelicopter()
-        if(gameObject===person) placePerson()
-    })
-}
-
-
-function update ()
-{
-    if(helicopterMove.move){
-        
-        if(helicopterMove.moveLeft){
-            helicopter.setScale(-0.5,0.5)
-            helicopter.setVelocityX(-100)
-        }else if(helicopterMove.moveRight){
-            helicopter.setScale(0.5,0.5)
-            helicopter.setVelocityX(100)
-        }
-        
-        if(helicopter.x>800){
-            helicopterMove.moveRight = false
-            helicopterMove.moveLeft = true
-        }
-        if(helicopter.x<200){
-            helicopterMove.moveRight = true
-            helicopterMove.moveLeft = false
-        }
-        
-        helicopter.anims.play("helicopter",true)
-        
+class Background{
+    constructor(position,size,color){
+        this.position = position
+        this.size = size
+        this.color = color
     }
 
-    if(carMove.move){
-        if(car.y>450){
-            car.setScale(3)
-        }else if(car.y>430){
-            car.setScale(2.5)
-        }else if(car.y>400){
-            car.setScale(2)
-        }else if(car.y>370){
-            car.setScale(1.6)
-        }else if(car.y>320){
-            car.setScale(-1.3,1.3)
-        }
+    draw(){
+        c.beginPath()
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.size.x, this.size.y)
         
-        if(carMove.forward){
-            car.anims.play("forward")
-            if(car.y>370){
-                car.setVelocityY(-20)
-                car.setVelocityX(-3)
-            }else if(car.y>330){
-                car.setVelocityY(-6)
-                car.setVelocityX(-5)
-            }else if(car.y<325){
-                carMove.forward = false
-                carMove.turnLeft = true
-            }
-        }
-
-        if(carMove.turnLeft){
-            car.setVelocityY(0)
-            car.setVelocityX(-30)
-            car.anims.play("forward",true)
-            if(car.x<500){
-                car.setVelocityY(0)
-                car.setVelocityX(0)
-                carMove.turnLeft = false
-                carMove.backwards = true
-            }
-        }
-
-        if(carMove.backwards){
-            car.anims.play("forward")
-            if(car.y<330){
-                car.setVelocityY(6)
-                car.setVelocityX(-20)
-            }else if(car.y<370){
-                car.setVelocityY(14)
-                car.setVelocityX(-10)
-            }else if(car.y<440){
-                car.setVelocityY(20)
-                car.setVelocityX(-10)
-            }
-            else if(car.y<480){
-                car.setVelocityY(0)
-                car.setVelocityX(0)
-                carMove.backwards = false
-                carMove.forward = true
-                car.x = 600
-                car.y = 500
-            }
-        }
-    }
-    
-}
-
-
-
-
-function placeCar(){
-    if(car.x>200 && car.x<780 && car.y>70 && car.y<520){
-        car.x = 600
-        car.y = 500
-        car.disableInteractive()
-        carMove.move = true
-        carMove.forward = true
-    }else{
-        car.x = 100
-        car.y = 100
     }
 }
 
-function placeHelicopter(){
-    if(helicopter.x>200 && helicopter.x<780 && helicopter.y>70 && helicopter.y<520){
-        helicopter.x = 600
-        helicopter.y = 200
-        helicopter.disableInteractive()
-        helicopterMove.move = true
-        helicopterMove.moveRight = true
-    }else{
-        helicopter.x = 100
-        helicopter.y = 170
-    }
-}
 
-function placePerson(){
-    if(person.x>200 && person.x<780 && person.y>70 && person.y<520){
-        person.disableInteractive()
-        person.anims.play("person")
-        if(person.x>500){
-            person.x = 700
-            person.setScale(-0.5,0.5)
+
+
+
+//colors
+const skyBg = c.createLinearGradient(0,0,0,canvas.height)
+skyBg.addColorStop(0, "rgb(0,0,100)")
+skyBg.addColorStop(0.5, "rgb(100,100,200)")
+skyBg.addColorStop(1, "white")
+
+const groundBg = c.createLinearGradient(0,300,0,canvas.height)
+groundBg.addColorStop(0, "rgb(110,45,10)")
+groundBg.addColorStop(0.5, "rgb(150,50,20)")
+groundBg.addColorStop(1, "rgb(190,105,40)")
+
+const sunBg = c.createRadialGradient(100,70,30,100,70,50)
+sunBg.addColorStop(0, "white")
+sunBg.addColorStop(1, "rgba(250,250,0,0)")
+
+//drawing sky and ground, we are using gradient colors defined above
+const sky = new Background({x:0,y:0},{x:canvas.width,y:300},skyBg)
+const ground = new Background({x:0,y:300},{x:canvas.width,y:300},groundBg)
+
+
+
+
+
+//animations
+let counter = 0
+
+class Pikachu{
+    constructor(){
+        this.x = 100
+        this.y = 400
+        this.sizeX = 150
+        this.sizeY = 100
+        this.images = {
+            pikachuRunning: createImage("./images/pikachu-running.png"),
+            angryPikachu: createImage("./images/angry-pikachu.png"),
+            deadPikachu: createImage("./images/dead-pikachu.png")
+        }
+        this.currentImage = this.images.deadPikachu
+        this.currentFrame = 0
+    }
+
+    draw(){
+        if(counter===200 || counter===400){
+            this.currentFrame = 0
+        }
+        if(counter<200){
+            this.currentImage = this.images.pikachuRunning
+        }else if(counter<400){
+            this.currentImage = this.images.angryPikachu
         }else{
-            person.x = 300
-            person.setScale(0.5)
+            this.currentImage = this.images.deadPikachu
         }
-        
-        person.y = 350
-    }else{
-        person.x = 100
-        person.y = 250
+
+        counter++
+        if(this.currentImage === this.images.pikachuRunning){
+            this.x++
+            c.drawImage(this.currentImage,Math.floor(this.currentFrame)*450,0,450,321,this.x,this.y,this.sizeX,this.sizeY)
+            if(this.currentFrame<3){
+                this.currentFrame+=0.1
+            }else{
+                this.currentFrame = 0
+            }
+        }
+
+        if(this.currentImage === this.images.angryPikachu){
+            c.save()
+            c.scale(-1,1)
+            c.drawImage(this.currentImage,Math.floor(this.currentFrame)*1750,0,1750,1183,-this.x,this.y-10,-this.sizeX-60,this.sizeY+30)
+            if(this.currentFrame<6){
+                this.currentFrame+=0.07
+            }else{
+                this.currentFrame = 0
+            }
+            c.restore()
+        }
+
+        if(this.currentImage === this.images.deadPikachu){
+            c.drawImage(this.currentImage,Math.floor(this.currentFrame)*200,0,200,157,this.x+20,this.y+40,this.sizeX-40,this.sizeY-40)
+            if(this.currentFrame<8){
+                this.currentFrame+=0.07
+            }
+            
+        }
     }
+}
+
+class Raichu{
+    constructor(){
+        this.x = 900
+        this.y = 400
+        this.sizeX = 150
+        this.sizeY = 100
+        this.images = {
+            pikachuRunning: raichuIdle("./images/raichu-idle.png"),
+            angryPikachu: createImage("./images/raichu-dead.png")
+        }
+        this.currentImage = this.images.deadPikachu
+        this.currentFrame = 0
+    }
+
+    draw(){
+        
+    }
+}
+
+const pikachu = new Pikachu()
+
+
+const image = new Image()
+image.src = "./images/dead-pikachu.png"
+image.addEventListener("load", ()=>{
+    c.drawImage(image,1200,0,200,157,600,300,200,200)
+})
+
+
+function animate(){
+    c.clearRect(0,0,canvas.width,canvas.height)
+    //drawing sky
+    sky.draw()
+
+    //drawing sun
+    c.fillStyle = sunBg
+    c.arc(100,70,50,0,Math.PI*2)
+    c.fill()
+
+    //drawing ground trees
+    ground.draw()
+   // treeArray.forEach(tree=>tree.draw())
+
+
+    requestAnimationFrame(animate)
+
+    pikachu.draw()
+    
+}
+
+animate()
+
+
+function createImage(src){
+    const image = new Image()
+    image.src = src
+    return image
 }
